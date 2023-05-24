e0984992d495d9036e35d5d6c5de940d80741f7c
darkhalkral <84571980+darkhalkral@users.noreply.github.com>
2023-05-25 01:25:15 +0300

    Add files via upload

---
+++ ders4/ders4/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ders4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar3 = new System.Windows.Forms.VScrollBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar2 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar3 = new System.Windows.Forms.HScrollBar();
+            this.hScrollBar4 = new System.Windows.Forms.HScrollBar();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(22, 38);
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(51, 151);
+            this.vScrollBar1.TabIndex = 0;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar1_Scroll);
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.Location = new System.Drawing.Point(73, 38);
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(53, 151);
+            this.vScrollBar2.TabIndex = 1;
+            this.vScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar2_Scroll);
+            // 
+            // vScrollBar3
+            // 
+            this.vScrollBar3.Location = new System.Drawing.Point(126, 38);
+            this.vScrollBar3.Name = "vScrollBar3";
+            this.vScrollBar3.Size = new System.Drawing.Size(52, 151);
+            this.vScrollBar3.TabIndex = 2;
+            this.vScrollBar3.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar3_Scroll);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.Location = new System.Drawing.Point(27, 197);
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(165, 47);
+            this.hScrollBar1.TabIndex = 3;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar1_Scroll);
+            // 
+            // hScrollBar2
+            // 
+            this.hScrollBar2.Location = new System.Drawing.Point(27, 255);
+            this.hScrollBar2.Name = "hScrollBar2";
+            this.hScrollBar2.Size = new System.Drawing.Size(165, 47);
+            this.hScrollBar2.TabIndex = 4;
+            this.hScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar2_Scroll);
+            // 
+            // hScrollBar3
+            // 
+            this.hScrollBar3.Location = new System.Drawing.Point(27, 311);
+            this.hScrollBar3.Name = "hScrollBar3";
+            this.hScrollBar3.Size = new System.Drawing.Size(165, 47);
+            this.hScrollBar3.TabIndex = 5;
+            this.hScrollBar3.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar3_Scroll);
+            // 
+            // hScrollBar4
+            // 
+            this.hScrollBar4.Location = new System.Drawing.Point(27, 358);
+            this.hScrollBar4.Name = "hScrollBar4";
+            this.hScrollBar4.Size = new System.Drawing.Size(165, 47);
+            this.hScrollBar4.TabIndex = 6;
+            this.hScrollBar4.Scroll += new System.Windows.Forms.ScrollEventHandler(this.hScrollBar4_Scroll);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(195, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(593, 382);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.hScrollBar4);
+            this.Controls.Add(this.hScrollBar3);
+            this.Controls.Add(this.hScrollBar2);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.vScrollBar3);
+            this.Controls.Add(this.vScrollBar2);
+            this.Controls.Add(this.vScrollBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.VScrollBar vScrollBar3;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar2;
+        private System.Windows.Forms.HScrollBar hScrollBar3;
+        private System.Windows.Forms.HScrollBar hScrollBar4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ders4/ders4/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ders4
+{
+    public partial class Form1 : Form
+    {
+
+        double R, G, B, C, M, Y, K, Rc, Gc, Bc = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void rgb()
+        {
+            R = vScrollBar1.Value;
+            G = vScrollBar2.Value;
+            B = vScrollBar3.Value;
+            if (R == 0 && G == 0 && B == 0)
+            {
+                C = 0;
+                M = 0;
+                Y = 0;
+                K = 0;
+            }
+            else
+            {
+                vScrollBar1.Value = Convert.ToInt32(Math.Round(R));
+                vScrollBar2.Value = Convert.ToInt32(Math.Round(G));
+                vScrollBar3.Value = Convert.ToInt32(Math.Round(B));
+                Rc = R / 255;
+                Gc = G / 255;
+                Bc = B / 255;
+                K = 1 - Math.Max(Math.Max(Rc, Gc), Bc);
+                C = (1 - Rc - K) / (1 - K) * 100;
+                M = (1 - Gc - K) / (1 - K) * 100;
+                Y = (1 - Bc - K) / (1 - K) * 100;
+                hScrollBar1.Value = Convert.ToInt32(C);
+                hScrollBar2.Value = Convert.ToInt32(M);
+                hScrollBar3.Value = Convert.ToInt32(Y);
+                hScrollBar4.Value = Convert.ToInt32(K);
+            }
+        }
+        private void cmyk()
+        {
+            C = hScrollBar1.Value;
+            M = hScrollBar2.Value;
+            Y = hScrollBar3.Value;
+            K = hScrollBar4.Value;
+            R = 255 * (1 - (C / 100)) * (1 - (K / 100));
+            G = 255 * (1 - (M / 100)) * (1 - (K / 100));
+            B = 255 * (1 - (Y / 100)) * (1 - (K / 100));
+            vScrollBar1.Value = Convert.ToInt32(Math.Round(R));
+            vScrollBar2.Value = Convert.ToInt32(Math.Round(G));
+            vScrollBar3.Value = Convert.ToInt32(Math.Round(B));
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          vScrollBar1.Minimum = 0;
+          vScrollBar1.Maximum = 255;
+          vScrollBar1.LargeChange = 1;
+          vScrollBar2.Minimum = 0;
+          vScrollBar2.Maximum = 255;
+          vScrollBar2.LargeChange = 1;
+          vScrollBar3.Minimum = 0;
+          vScrollBar3.Maximum = 255;
+          vScrollBar3.LargeChange = 1;
+          hScrollBar1.Minimum = 0;
+          hScrollBar1.Maximum = 100;
+          hScrollBar1.LargeChange = 1;
+          hScrollBar2.Minimum = 0;
+          hScrollBar2.Maximum = 100;
+          hScrollBar2.LargeChange = 1;
+          hScrollBar3.Minimum = 0;
+          hScrollBar3.Maximum = 100;
+          hScrollBar3.LargeChange = 1;
+          hScrollBar4.Minimum = 0;
+          hScrollBar4.Maximum = 100;
+          hScrollBar4.LargeChange = 1;
+        }
+        private void vScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {        
+            rgb();           
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(vScrollBar1.Value, vScrollBar2.Value, vScrollBar3.Value);
+        }
+
+        private void vScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {          
+            rgb();           
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(vScrollBar1.Value, vScrollBar2.Value, vScrollBar3.Value);
+        }
+        private void vScrollBar3_Scroll(object sender, ScrollEventArgs e)
+        {          
+            rgb();
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(vScrollBar1.Value, vScrollBar2.Value, vScrollBar3.Value);
+        }
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            cmyk();
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(Convert.ToInt32(R), Convert.ToInt32(G), Convert.ToInt32(B));
+        }
+
+        private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
+        {
+            cmyk();
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(Convert.ToInt32(R), Convert.ToInt32(G), Convert.ToInt32(B));
+        }
+
+        private void hScrollBar3_Scroll(object sender, ScrollEventArgs e)
+        {
+            cmyk();
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(Convert.ToInt32(R), Convert.ToInt32(G), Convert.ToInt32(B));
+        }
+
+        private void hScrollBar4_Scroll(object sender, ScrollEventArgs e)
+        {
+            cmyk();
+            pictureBox1.BackColor = System.Drawing.Color.FromArgb(Convert.ToInt32(R), Convert.ToInt32(G), Convert.ToInt32(B));
+        }
+    }
+}
